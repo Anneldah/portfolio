96917df07b874616c9f311c26f92d7ad96d3b0b6
Ncedo <anneldahnce@gmail.com>
2022-11-10 19:39:37 +0200

    imported styles on the api  file

---
+++ api/_app.js
@@ -0,0 +1,11 @@
+import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.mi.css';
+import 'remixicon/fonts/remixicon.css'
+import "slick-carousel/slick/slickcss";
+import "slick-carousel/slick/slick-theme.css";
+
+function MyApp({ component, pageProps}){
+    return <Compnent {...pageProps}/>
+}
+
+export default MyApp
